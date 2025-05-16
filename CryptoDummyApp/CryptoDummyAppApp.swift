ce6854e912d733c5a87e3951cb91a730c6ed7da0
emrebingor <emrebingor5@gmail.com>
2025-05-16 12:47:13 +0300

    Initial Commit

---
+++ CryptoDummyApp/CryptoDummyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoDummyAppApp.swift
+//  CryptoDummyApp
+//
+//  Created by Emre Bingor on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoDummyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
